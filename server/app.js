77a3e52c9328272d0f223a42f424cced09f4a1f5
somsomy <yun07003@naver.com>
2022-09-09 15:25:49 +0900

    initial commit (test)

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const dotenv = require("dotenv");
+const express = require('express');
+
+dotenv.config();
+global.WebSocket = require("isomorphic-ws");
+const { Game } = require('@gathertown/gather-game-client');
+
+const app = express();
+
+const API_KEY = process.env.GATHER_API_KEY;
+const SPACE_ID = process.env.GATHER_SPACE_ID;
+
+const game = new Game(SPACE_ID, () => Promise.resolve({ apiKey: API_KEY }));
+// this is the line that actually connects to the server and starts initializing stuff
+game.connect();
+// optional but helpful callback to track when the connection status changes
+game.subscribeToConnection((connected) => console.log("connected?", connected));
+game.subscribeToEvent("playerJoins", (player) => {
+  console.log(Object.keys(game.players).length);
+});
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get("/count", (req, res) => { res.json({ count: Object.keys(game.players).length})});
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+
+app.listen(8000,
+  () => console.log("Server is running..."));
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const host = process.env.GATHER_API_HOST;
+  axios.get(`${host}/count`).then((res) => {
+    setCount(res.data.count);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>1111111 /// {count}</h1>
     </div>
   );
 }
